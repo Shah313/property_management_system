c75e75a7b60073962ed6fc9b75863e866867259c
Shah <shoaibshah1255@gmail.com>
2025-01-09 18:57:56 -0500

    add patches

--- property_management_system/patches/create_custom_fields.py
+++ property_management_system/patches/create_custom_fields.py
@@ -4,11 +4,37 @@ def execute():
     custom_fields = {
         "Quotation": [
             {
-                "fieldname": "custom_tenant",
-                "label": "Tenant",
+                "fieldname": "custom_property_name",
+                "label": "Property Name",
                 "fieldtype": "Link",
-                "options": "Tenant",
+                "options": "Property",
                 "insert_after": "naming_series",
+                "reqd": 1,
+            },
+            {
+                "fieldname": "custom_property_unit",
+                "label": "Property Unit",
+                "fieldtype": "Link",
+                "options": "Home Unit",
+                "insert_after": "custom_property_name",
+                "reqd": 1,
+            },
+        ],
+        "Sales Order": [
+            {
+                "fieldname": "custom_property_name",
+                "label": "Property Name",
+                "fieldtype": "Link",
+                "options": "Property",
+                "insert_after": "naming_series",
+                "reqd": 1,
+            },
+            {
+                "fieldname": "custom_property_unit",
+                "label": "Property Unit",
+                "fieldtype": "Link",
+                "options": "Home Unit",
+                "insert_after": "custom_property_name",
                 "reqd": 1,
             },
         ]
@@ -21,4 +47,6 @@ def execute():
                 custom_field.dt = doctype
                 custom_field.update(field)
                 custom_field.insert()
+
+    # Clear cache to ensure fields are loaded in the UI
     frappe.clear_cache()
