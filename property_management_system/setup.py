f86df03f9d9635d2a39d8d6df780c2e82c2704f7
Shah <shoaibshah1255@gmail.com>
2025-01-09 18:32:42 -0500

    testing

--- property_management_system/setup.py
+++ property_management_system/setup.py
@@ -8,23 +8,23 @@ def setup_custom_fields():
                 "label": "Property Name",
                 "fieldtype": "Link",
                 "options": "Property",
-                "insert_after": "naming_series",  
-                "reqd": 1,  
+                "insert_after": "naming_series",
+                "reqd": 1,
             },
             {
                 "fieldname": "custom_property_unit",
                 "label": "Property Unit",
                 "fieldtype": "Link",
                 "options": "Home Unit",
-                "insert_after": "custom_property_name", 
-                "reqd": 1,  
+                "insert_after": "custom_property_name",
+                "reqd": 1,
             },
             {
                 "fieldname": "custom_property_owner",
                 "label": "Property Owner",
                 "fieldtype": "Link",
                 "options": "Owner",
-                "insert_after": "custom_property_unit",  
+                "insert_after": "custom_property_unit",
                 "reqd": 1,
             },
         ],
@@ -35,7 +35,7 @@ def setup_custom_fields():
                 "fieldtype": "Link",
                 "options": "Property",
                 "insert_after": "naming_series",
-                "reqd": 1,  
+                "reqd": 1,
             },
             {
                 "fieldname": "custom_property_unit",
@@ -43,7 +43,7 @@ def setup_custom_fields():
                 "fieldtype": "Link",
                 "options": "Home Unit",
                 "insert_after": "custom_property_name",
-                "reqd": 1,  
+                "reqd": 1,
             },
         ]
     }
@@ -51,6 +51,9 @@ def setup_custom_fields():
     for doctype, fields in custom_fields.items():
         for field in fields:
             create_custom_field(doctype, field)
+    
+    # Clear cache to ensure fields are loaded in the UI
+    frappe.clear_cache()
 
 def create_custom_field(doctype, field):
     if not frappe.db.exists("Custom Field", {"dt": doctype, "fieldname": field["fieldname"]}):
