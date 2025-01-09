8d8a80d9318c1265f5efa7d5ce7bf4f842903861
Shah <shoaibshah1255@gmail.com>
2025-01-09 14:36:47 -0500

    updated setup.py

---
+++ property_management_system/setup.py
@@ -0,0 +1,61 @@
+import frappe
+
+def setup_custom_fields():
+    custom_fields = {
+        "Quotation": [
+            {
+                "fieldname": "custom_property_name",
+                "label": "Property Name",
+                "fieldtype": "Link",
+                "options": "Property",
+                "insert_after": "Series",
+                "reqd": 1,  
+            },
+            {
+                "fieldname": "custom_property_unit",
+                "label": "Property Unit",
+                "fieldtype": "Link",
+                "options": "Home Unit",
+                "insert_after": "Property Name",
+                "reqd": 1,  
+            },
+               {
+                "fieldname": "custom_property_owner",
+                "label": "Property Owner",
+                "fieldtype": "Link",
+                "options": "Owner",
+                "insert_after": "Property Unit",
+                "reqd": 1,  
+            },
+        ],
+        "Sales Order": [
+            {
+                "fieldname": "custom_property_name",
+                "label": "Property Name",
+                "fieldtype": "Link",
+                "options": "Property",
+                "insert_after": "Series",
+                "reqd": 1,  
+            },
+            {
+                "fieldname": "custom_property_unit",
+                "label": "Property Unit",
+                "fieldtype": "Link",
+                "options": "Home Unit",
+                "insert_after": "Property Name",
+                "reqd": 1,  
+            },
+        ]
+    }
+
+    for doctype, fields in custom_fields.items():
+        for field in fields:
+            create_custom_field(doctype, field)
+
+def create_custom_field(doctype, field):
+    if not frappe.db.exists("Custom Field", {"dt": doctype, "fieldname": field["fieldname"]}):
+        custom_field = frappe.new_doc("Custom Field")
+        custom_field.dt = doctype
+        custom_field.__dict__.update(field)
+        custom_field.insert()
+        frappe.db.commit()
